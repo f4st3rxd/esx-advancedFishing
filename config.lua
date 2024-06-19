40b19b97b5377daa306b96b8e02833e38696dbd7
f4st3r <152923524+f4st3rxd@users.noreply.github.com>
2024-06-19 21:29:40 +0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,9 @@
+FastConfig = {}
+
+FastConfig.OltaMiktar = 2500
+FastConfig.YemMiktar = 100
+FastConfig.KamplumbagaYem = 250
+
+FastConfig.FishTime = {a = 10000, b = 24000}
+
+FastConfig.BalikFiyat = 450
---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+author 'kuzkay / translated by f4st3r'
+description 'esx-advancedFishing'
+fx_version 'cerulean'
+game 'gta5'
+
+client_scripts {
+  'client/client.lua'
+}
+
+server_scripts { 
+	'server/server.lua'
+}
+
+shared_scripts {
+  'config.lua'
+}
+
+
